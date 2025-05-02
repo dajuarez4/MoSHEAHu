47dc4bfe42c5bf3aab4684701ce2ad87265ffe06
Diego Armando Juarez Rosales <diego.juarez@fisica.uaz.edu.mx>
2025-05-01 19:04:25 -0600

    update utils

--- utils/initial_parameters.py
+++ utils/initial_parameters.py
@@ -1,23 +1,23 @@
 import numpy as np
 
-simulation_path = './simulations'
-potential_path  = './potentials/'
+simulation_path = './simulations/sim_'
+potential_path  = '/Users/dajuarez4/HEAS/potentials/'#'./potentials/'
 executable_path = './executables/'
 pot_files = ['library_HfNbTaTiZr.meam','HfNbTaTiZr.meam']
 name_hea = 'HfNbTaTiZr' #HfNbTaTiZr.xyz
 lmp_mpi = 'lmp_mpi'
-cuttof = 2.0
+cuttof = 2.5
 system_size = 20
 cs = 'bcc'
 atom_types = ['Hf', 'Nb', 'Ta', 'Ti', 'Zr']
 num_types = len(atom_types)
-initial_lattice = 3.38
-n_latti_par = 1
-n_sim = 2
+initial_lattice = 3.30
+n_latti_par = 16
+n_sim = 4
 lattice_parameters = [np.round(initial_lattice + ii*(0.01), 2) for ii in range(n_latti_par)]
 simulation_paths = [simulation_path + f'{sim+1}/' for sim in range(n_sim)]
 input_file_path = './input_files/'
 input_files = ['HfNbTaTiZr.in']
+ids = []
 energies_1000 = []
 energies_2000 = []
-ids = []
